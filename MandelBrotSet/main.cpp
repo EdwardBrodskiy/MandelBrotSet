6dc2e81202b44774e27ab6f6896cebc1731a5976
EdwardBrodskiy <49645295+EdwardBrodskiy@users.noreply.github.com>
2020-06-22 12:39:24 +0100

    Main refactor

--- MandelBrotSet/main.cpp
+++ MandelBrotSet/main.cpp
@@ -5,16 +5,17 @@
 #include "Grid.h"
 #include "MandelBrot.h"
 
-Complex determine_new_center(sf::Vector2i mouse_pos, long double scale, Complex old_center);
+Complex mouse_pos_to_complex(sf::Vector2i mouse_pos, long double scale, Complex old_center);
 
 bool in_window(sf::Vector2i mouse_pos);
 
+// window resolution
 int height = 1080;
 int width = 1920;
 
 int main() {
 
-	MandelBrot mb(width, height);
+	MandelBrot mandelBrot(width, height);
 
 	sf::RenderWindow window(sf::VideoMode(width, height), "MandelBrotSet", sf::Style::Fullscreen);
 
@@ -46,30 +47,30 @@ int main() {
 			if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
 				mouse_pos = sf::Mouse::getPosition(window);
 				if (in_window(mouse_pos)) {
-					Complex new_center = determine_new_center(mouse_pos, mb.get_scale(), mb.get_center());
-					mb.set_center(new_center);
-					mb.change_scale_by(0.5);
+					Complex new_center = mouse_pos_to_complex(mouse_pos, mandelBrot.get_scale(), mandelBrot.get_center());
+					mandelBrot.set_center(new_center);
+					mandelBrot.change_scale_by(0.5);
 					window.clear();
 				}
 			}
 			else if (sf::Mouse::isButtonPressed(sf::Mouse::Right)) {
 				mouse_pos = sf::Mouse::getPosition(window);
 				if (in_window(mouse_pos)) {
-					Complex new_center = determine_new_center(mouse_pos, mb.get_scale(), mb.get_center());
-					mb.set_center(new_center);
-					mb.change_scale_by(2.0);
+					Complex new_center = mouse_pos_to_complex(mouse_pos, mandelBrot.get_scale(), mandelBrot.get_center());
+					mandelBrot.set_center(new_center);
+					mandelBrot.change_scale_by(2.0);
 					window.clear();
 				}
 
 			}// Keyboard input
 			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Equal)) {
-				mb.change_itterations_by(128);
+				mandelBrot.change_itterations_by(128);
 			}
 			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Dash)) {
-				mb.change_itterations_by(-128);
+				mandelBrot.change_itterations_by(-128);
 			}
 			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::B)) {
-				image.update(mb.get_buddah_brot());
+				image.update(mandelBrot.get_buddah_brot());
 				window.draw(sprite);
 				window.display();
 			}
@@ -83,8 +84,8 @@ int main() {
 		
 		
 		// Processes 
-		if (mb.is_active()) {
-			image.update(mb.feed_result_of_next_activity());
+		if (mandelBrot.is_active()) { // check if mandel brot has a new render to display
+			image.update(mandelBrot.feed_result_of_next_activity());
 			window.draw(sprite);
 			window.display();
 		}
@@ -95,7 +96,8 @@ int main() {
 	}
 }
 
-Complex determine_new_center(sf::Vector2i mouse_pos, long double scale, Complex old_center) {
+/* convert mouse cooridnates to complex coordiante on screen */
+Complex mouse_pos_to_complex(sf::Vector2i mouse_pos, long double scale, Complex old_center) {
 	Complex new_center;
 	new_center.real = mouse_pos.x * 2.f * scale / width - scale + old_center.real;
 	new_center.imaginary = mouse_pos.y * 2.f * scale / height - scale + old_center.imaginary;
@@ -103,6 +105,7 @@ Complex determine_new_center(sf::Vector2i mouse_pos, long double scale, Complex 
 
 }
 
+/* check if mouse position is with in the screen */
 bool in_window(sf::Vector2i mouse_pos) {
 	if (0 <= mouse_pos.x and mouse_pos.x <= width ){
 		if (0 <= mouse_pos.y and mouse_pos.y <= height) {
